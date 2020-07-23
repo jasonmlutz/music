8e9d4874138feb855ad82df4b5d11058b93ac895
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-23 12:13:28 -0700

    Create SessionsController and new + create methods & associated views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      render json: 'invalid credentials'
+    else
+      user.reset_session_token!
+      login!(user)
+      redirect_to user_url(user)
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
+
+end
