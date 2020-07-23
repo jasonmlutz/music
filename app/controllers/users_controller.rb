d0504767e482e52b76aedd5d9221e4264630a6b4
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-23 11:04:04 -0700

    Create controller and methods for User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save!
+      redirect_to user_url(user)
+    else
+      redirect_to new_user_url
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    if user
+      render json: user
+    else
+      redirect_to new_user_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
