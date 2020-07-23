d12cf327595efa323ea9e52a929523dde94ff159
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-23 10:38:32 -0700

    Create password related methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,11 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-  before_validation :ensure_session_token
+  attr_reader :password
+  after_initialize :ensure_session_token
   validates :email, :password_digest, :session_token, null: false
   validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
 
   def self.generate_session_token
     SecureRandom.urlsafe_base64
@@ -26,6 +28,20 @@ class User < ApplicationRecord
 
   def ensure_session_token
     self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user.nil? || !user.is_password?(password) ? nil : user
   end
 
 end
