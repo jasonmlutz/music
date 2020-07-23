a1c4a0896cc2a3274f47fe8d86aaf92c194524eb
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-23 12:09:13 -0700

    Create + update login/logout helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def logged_in?(user)
+    current_user == user
+  end
+
+  def login!(user)
+    @current_user = user
+    session[:session_token] = user.session_token
+  end
+
+  def logout!
+    @current_user = nil
+    session[:session_token] = nil
+  end
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    return nil if session[:session_token].nil?
+
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
 end
