165875193a7d0c0e022b85cce1f3074c9ec0d358
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-23 10:22:19 -0700

    Create session_token methods and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,22 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  before_validation :ensure_session_token
   validates :email, :password_digest, :session_token, null: false
   validates :email, uniqueness: true
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 
 end
