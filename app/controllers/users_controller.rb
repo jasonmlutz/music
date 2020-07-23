033b10b958250df084c27310207ebe06ee0eea27
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-23 12:10:06 -0700

    Include login! call in UsersController#create and update #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save!
+      login!(user)
       redirect_to user_url(user)
     else
       redirect_to new_user_url
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   def show
     user = User.find_by(id: params[:id])
     if user
-      render json: user
+      render :show
     else
       redirect_to new_user_url
     end
