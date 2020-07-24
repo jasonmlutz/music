14e7c95d1ce8d629549cf8e01b232ae37bd0d4dd
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-23 21:08:01 -0700

    Add AlbumsController #edit and #update methods ...
    and associated views; format _album_form partial radio buttons; reorganize order of AlbumsController methods

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,21 @@
 class AlbumsController < ApplicationController
+  def new
+    @album = Album.new
+    @album_artist = Band.find_by(id: params[:band_id])
+    @bands = Band.all
+    render :new
+  end
+
+  def create
+    fail
+    @album = Album.new(album_params)
+    if @album.save!
+      redirect_to album_url(@album)
+    else
+      redirect_to band_url(Band.find_by(id: @album.band_id))
+    end
+  end
+
   def show
     @album = Album.find_by(id: params[:id])
     if @album
@@ -8,25 +25,28 @@ class AlbumsController < ApplicationController
     end
   end
 
-  def new
-    @album = Album.new
-    @album_artist = Band.find_by(id: params[:band_id])
+  def edit
+    @album = Album.find_by(id: params[:id])
     @bands = Band.all
-    render :new
+    if @album
+      @album_artist = Band.find_by(id: @album.band_id)
+      render :edit
+    else
+      redirect_to bands_url
+    end
   end
 
-  def create
-    @album = Album.new(album_params)
-    if @album.save!
+  def update
+    @album = Album.find_by(id: params[:id])
+    if @album.update(album_params)
       redirect_to album_url(@album)
     else
-      redirect_to band_url(Band.find_by(id: @album.band_id))
+      render :edit
     end
-
   end
 
   private
   def album_params
-    params.require(:album).permit(:title, :year, :band_id)
+    params.require(:album).permit(:title, :year, :band_id, :studio)
   end
 end
