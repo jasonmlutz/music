65ea27f6d15a12f1073038607cd82b00807985f8
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-24 11:29:18 -0700

    Create outline of TracksController

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,31 @@
+class TracksController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:album_id, :title, :ord, :lyrics, :regular)
+  end
+end
