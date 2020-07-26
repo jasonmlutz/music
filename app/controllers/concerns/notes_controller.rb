36448e99b1d076279da67f3fc8e194d724164212
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-25 21:19:03 -0700

    Include destroy method; add destroy button to note listing

--- app/controllers/concerns/notes_controller.rb
+++ app/controllers/concerns/notes_controller.rb
@@ -10,6 +10,17 @@ class NotesController < ApplicationController
   end
 
   def destroy
+    note = Note.find_by(id: params[:id])
+    if note
+      if current_user.nil? || current_user.id != note.author_id
+        render text: 'you are not authorized to take that action'
+      else
+        note.destroy
+        redirect_to track_url(note.track_id)
+      end
+    else
+      redirect_to bands_url
+    end
 
   end
 
