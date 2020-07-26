d61fb3b1916b0c4f0d1a3b2ac8e0ee378f5d7a03
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-26 10:22:28 -0700

    Add lyrics display helper; include helper in lyrics #show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
+  include ERB::Util
+
   def auth_token
     '<input type="hidden" name="authenticity_token" value="#{form_authenticity_token}" />'.html_safe
+  end
+
+  def ugly_lyrics(lyrics)
+    lines = lyrics.split("\n")
+    output = ''
+    lines.each do |line|
+      output += ( line.length > 1 ? '&#9835; ' : '') + line + "\n"
+    end
+    output.html_safe
   end
 end
