36dd911ceeff0066e8e40237288f90978cd44df4
Jason Lutz <jason.m.lutz@gmail.com>
2020-07-26 10:51:18 -0700

    Replace existing vars with instance vars in NotesController#create

--- app/controllers/concerns/notes_controller.rb
+++ app/controllers/concerns/notes_controller.rb
@@ -1,11 +1,12 @@
 class NotesController < ApplicationController
   def create
-    note = Note.new(note_params)
-    note.author_id = current_user.id
-    if note.save
-      redirect_to track_url(note.track_id)
+    @note = Note.new(note_params)
+    @note.author_id = current_user.id
+    if @note.save
+      redirect_to track_url(@note.track_id)
     else
-      redirect_to track_url(note.track_id)
+      flash[:errors] = @note.errors.full_messages
+      redirect_to track_url(@note.track_id)
     end
   end
 
