825760bc8bc0ed66d1988a8aac04af797e6a36a5
Jason Lutz <jason.m.lutz@gmail.com>
2020-08-01 13:03:39 -0700

    Remove email uniqueness validation check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,4 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:password_digest) }
   it { should validate_presence_of(:session_token) }
   it { should validate_length_of(:password).is_at_least(6) }
-  it { should validate_uniqueness_of(:email) }
 end
